6d09ff19ef18e14254b078dd4059c7dfae357241
Ian Buchanan <ibuchanan@atlassian.com>
2019-08-11 15:16:08 -0500

    add flask configuration and debug logging

---
+++ .flaskenv
@@ -0,0 +1,1 @@
+FLASK_APP=jira-clone.py
--- jira-clone.py
+++ jira-clone.py
@@ -1,16 +1,21 @@
+import logging
 from flask import Flask, request, make_response, jsonify
 app = Flask(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
 
 @app.route('/', methods = ['POST'])
 def webhook():
+    logging.debug('received webhook')
     try:
         req = request.get_json()
-        return make_response('', status.HTTP_204_NO_CONTENT)
+        logging.debug(req)
+        return make_response('', 204)
     except Exception as e:
-        http_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        http_code = 500
 
-    return make_response(jsonify(message='Webhook failed', error=e.message), http_code) 
+    return make_response(jsonify(message='Webhook failed', error=e.message), http_code)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
+    logging.debug('waiting for webhook')
